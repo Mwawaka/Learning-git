44a50e632e19c239fa8ab2a5bd3371e4f23ab037
Mwawaka <kedskeds90@gmail.com>
2022-02-09 10:46:11 +0300

    Virus with its own space

--- hacker.py
+++ hacker.py
@@ -1,5 +1,5 @@
 messages='I am Enigma the code decrypter!Haha intercepted and decrypted your code'
-malware=['Trojan Horse','Spyware','CryptoLocker','Nimda']
+malware=['Trojan Horse\n','Spyware\n','CryptoLocker\n','Nimda']
 a,b,c,d=malware
 print(a,b,c,d)
 print(len(malware))
