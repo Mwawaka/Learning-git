f9b54837aa49bef7b02c1f0b987453014c3d5b7e
Mwawaka <94539437+Mwawaka@users.noreply.github.com>
2022-02-09 10:39:55 +0300

    Virus upgrade

--- hacker.py
+++ hacker.py
@@ -1,2 +1,7 @@
-messages='I am Enigma the firewall cracker'
-print (messages)
+messages='I am Enigma the code decrypter!Haha intercepted and decrypted your code'
+malware=['Trojan Horse','Spyware','CryptoLocker','Nimda']
+a,b,c,d=malware
+print(a,b,c,d)
+print(len(malware))
+print(type(malware))
+print (messages) 
